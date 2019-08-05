1cabeab83abcb8d5d1ccbbc21d3a69e699d8785d
Isaac Rosenberg <irosenb7@gmail.com>
2019-08-05 17:27:55 -0400

    first version complete;

--- models/visit.js
+++ models/visit.js
@@ -20,20 +20,38 @@ Visit.create = function(userId, name, callback) {
   });
 }
 
-Visit.search = function(name = null, userId = null, id = null,  callback) {
-  var query
+Visit.search = function(name, userId, id,  callback) {
+  const client = Visit.connection();
+
+  var query;
   var params;
-  if (id !== null) {
+  if (id != null) {
+    console.log("id provided")
     query = {
       text: "SELECT * FROM Visits WHERE id = $1",
       values: [id]
-  }
-  } else if (name !== null && userId !== null) {
-    query = {
-      text: "SELECT * FROM Visits WHERE userId = $1 AND LIKE %$2%",
-      values: [userId, name]
     }
+  } else if (name !== null && userId !== null) {
+    console.log(name)
+    console.log("User ID: " + userId)
+    query = {
+      text: "SELECT * FROM Visits WHERE userId = $1 AND name LIKE $2 ORDER BY created_at DESC LIMIT 5",
+      values: [userId, '%' + name + '%']
+    }
+  } else {
+    callback(null, new Error("Please provide a user id and name, or an id"))
   }
+
+  client.query(query, function (err, results) {
+    if (err) {
+      console.log(err);
+      callback(null, err);
+    } else {
+      console.log(results);
+      callback(results.rows, null);
+    }
+    client.end();
+  })
 }
 
 Visit.connection = function() {
--- routes/visits.js
+++ routes/visits.js
@@ -5,17 +5,23 @@ var Visit = require('../models/visit.js');
 router.post('/', function(req, res, next) {
   Visit.create(req.body.userId, req.body.name, function(result, err) {
     if (err) {
-      res.status(400).send("error")
+      res.status(400).send("error");
     } else {
-      res.send({"id": result.id})
+      res.send({"id": result.id});
     }
-  })
-})
+  });
+});
 
 router.get('/', function(req, res, next) {
-  Visit.search(req.params.name, req.params.userId, req.params.id, function(results, err) {
-
-  })
-})
+  console.log(req);
+  console.log(req.params.id);
+  Visit.search(req.body.name, req.body.userId, req.body.id, function(results, err) {
+    if (err) {
+      res.status(400).send("error");
+    } else {
+      res.send(results);
+    }
+  });
+});
 
 module.exports = router;
